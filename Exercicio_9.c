084c9731373874f68b1d05ad5825ef976d261c16
Gleyson Willian Bruno Costa Rocha <157893544+GleysonWillian@users.noreply.github.com>
2024-10-09 20:46:26 -0300

    Exercicio0_.c commitado

--- Exercicio_9.c
+++ Exercicio_9.c
@@ -2,10 +2,19 @@
 
 int main()
 {
-    int num = 0;
-    float media = 0;
-    
+    int i = 1;
+    int num = 1;
+    int fat = 1;
 
+    printf("Digite um valor para num: ");
+    scanf("%d", &num);
+
+    for (i = num; i > 1; i--)
+    {
+        fat = fat * i;
+    }
+
+    printf("\nO valor do fatorial de %d e igual a %d \n .", num, fat);
 
     return 0;
 }
